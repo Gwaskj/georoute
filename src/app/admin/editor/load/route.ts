64631b7c871fcd69ff9d2bd67b2c381cb4465fce
matthew hailey <matthewphailey@gmail.com>
2026-05-03 17:22:15 +0100

    v1

--- src/app/admin/editor/load/route.ts
+++ src/app/admin/editor/load/route.ts
@@ -1,6 +1,3 @@
-export const dynamic = "force-static";
-export const fetchCache = "force-no-store";
-
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 import { createServerClient } from "@supabase/ssr";
