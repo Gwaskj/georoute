5129b6414fae8ebb74d9a1ef7ceba71c04c568ae
matthew hailey <matthewphailey@gmail.com>
2026-05-31 23:49:24 +0100

    V0.7.6 N16

--- next.config.js
+++ next.config.js
@@ -2,18 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
 
-  // ⭐ Allow importing Leaflet PNGs
   images: {
     unoptimized: true,
   },
 
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(png|jpg|jpeg|gif|svg)$/i,
-      type: "asset/resource",
-    });
-    return config;
-  },
+  // Enable Turbopack explicitly (silences the warning)
+  turbopack: {},
 };
 
 module.exports = nextConfig;
